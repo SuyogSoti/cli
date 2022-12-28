17bd67dfd364bdd555989e147a8a3b3024a32e97
Suyog Soti <suyog.soti@gmail.com>
2022-12-27 19:36:11 -0800

    changed commands to be more realistic

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
-mod commands;
+mod worktree;
 mod errors;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
-struct Args {
-    cmd: String,
+struct Cli {
+    #[command(subcommand)]
+    cmd: TopLevelCmds,
+}
+
+#[derive(Subcommand)]
+enum TopLevelCmds {
+    WT {
+        #[command(subcommand)]
+        wt_cmd: worktree::WortreeCommands,
+    },
 }
 
 fn main() {
-    let args: Args = Args::parse();
-    let result = commands::top_level_commands(args.cmd)
-        .map(|cmd| cmd.run());
-
+    let cli: Cli = Cli::parse();
+    let result = match cli.cmd {
+        TopLevelCmds::WT { wt_cmd } => worktree::worktree(wt_cmd),
+    };
     match result {
         Err(err) => println!("Error executing command: {}", err.to_string()),
         _ => (),
---
+++ src/worktree.rs
@@ -0,0 +1,37 @@
+use crate::errors::Error;
+use clap::Subcommand;
+
+#[derive(Subcommand)]
+pub enum WortreeCommands {
+    ADD { branch: String },
+    DELETE { branch: String },
+}
+
+pub fn worktree(wt_cmd: WortreeCommands) -> Result<(), Error> {
+    match wt_cmd {
+        WortreeCommands::ADD { branch } => worktree_add_branch(branch),
+        WortreeCommands::DELETE { branch } => worktree_delete_branch(branch),
+    }
+}
+
+fn worktree_add_branch(_branch: String) -> Result<(), Error> {
+    let result = git2::Repository::open(".");
+    match result {
+        Ok(repo) => {
+            let path = repo.path().display().to_string();
+            println!("{}", path);
+            return Ok(());
+        }
+        Err(err) => Err(Error::new(err.message())),
+    }
+}
+
+fn worktree_delete_branch(branch: String) -> Result<(), Error> {
+    let result = git2::Repository::open(".").and_then(|repo| repo.find_worktree(branch.as_str()));
+    match result {
+        Ok(_path) => {
+            return Ok(());
+        }
+        Err(err) => Err(Error::new(err.message())),
+    }
+}
