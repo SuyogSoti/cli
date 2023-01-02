35135e64500dc0b9df96bebfa4a0ce998740ec95
Suyog Soti <suyog.soti@gmail.com>
2023-01-01 16:10:44 -0800

    add code to find top level bare repo

--- src/worktree.rs
+++ src/worktree.rs
@@ -14,16 +14,29 @@ pub fn worktree(wt_cmd: WortreeCommands) -> Result<(), Error> {
     }
 }
 
-fn worktree_add_branch(_branch: String) -> Result<(), Error> {
-    let result = git2::Repository::open(".");
-    match result {
-        Ok(repo) => {
-            let path = repo.path();
-            repo.workdir().map(|idx| println!("{}", idx.display()));
-            println!("{}", path.display());
-            return Ok(());
-        }
-        Err(err) => Err(Error::new(err.message())),
+fn get_repo_root(path: &std::path::PathBuf) -> Option<git2::Repository> {
+    let repo = git2::Repository::open(path);
+    match repo.as_ref().map(|r| r.is_bare()) {
+        Ok(true) => match repo {
+            Ok(repo) => Some(repo),
+            _ => None,
+        },
+        _ => match path.parent() {
+            Some(parent) => get_repo_root(&parent.to_path_buf()),
+            None => None,
+        },
+    }
+}
+
+fn worktree_add_branch(branch: String) -> Result<(), Error> {
+    let repo = std::env::current_dir().map(|pathbuf| get_repo_root(&pathbuf));
+    match repo {
+        Ok(Some(repo)) => Ok(println!(
+            "{} -  need to add {}",
+            repo.path().display(),
+            branch
+        )),
+        _ => Ok(()),
     }
 }
 
